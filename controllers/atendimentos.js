9688a2a249857f9f17007b326f862a1408e5a348
Almoliveira <almoliveira.info@gmail.com>
2021-12-23 10:16:12 -0300

    first commit

---
+++ controllers/atendimentos.js
@@ -0,0 +1,15 @@
+const Atendimento = require('../models/atendimento')
+
+module.exports = (app) => {
+  app.get("/atendimentos", (req, res) => {
+    return res.send(
+      "Você está na rota de atendimentos e está realizando um GET"
+    );
+  });
+
+  app.post("/atendimentos", (req, res) => {
+    const atendimento = req.body;
+    Atendimento.adiciona(atendimento);
+    console.log("POS Atendimento")
+  });
+};
